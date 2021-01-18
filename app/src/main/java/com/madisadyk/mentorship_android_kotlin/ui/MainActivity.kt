07b144f7c0577c211571e6f6326f73c397b64fcc
Madi Sadyk <sadykmadi27@gmail.com>
2021-01-18 19:47:20 +0600

    Initial commit

---
+++ app/src/main/java/com/madisadyk/mentorship_android_kotlin/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.madisadyk.mentorship_android_kotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.madisadyk.mentorship_android_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
